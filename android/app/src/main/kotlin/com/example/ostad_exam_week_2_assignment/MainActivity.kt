8fe4b85e9fb1288c51a9181b805887c38fd9190d
Shofiqur Rahman Soyon <shofiqur7684@gmail.com>
2024-11-10 01:29:30 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ostad_exam_week_2_assignment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ostad_exam_week_2_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Profile'),
+        backgroundColor: Colors.yellow,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.settings_sharp),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.call),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              CircleAvatar(
+                maxRadius: 80,
+                child: Icon(
+                  Icons.icecream_outlined,
+                  size: 70,
+                ),
+              ),
+              const SizedBox(height: 8,),
+              Text(
+                'Ice cream is very delicious right?',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 32,),
+              CircleAvatar(
+                maxRadius: 80,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.chevron_left,
+                      size: 70,
+                    ),
+                    Icon(
+                      Icons.chevron_right,
+                      size: 70,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 8,),
+              Text(
+                'Programming is not boring if you love it',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 32,),
+              CircleAvatar(
+                maxRadius: 70,
+                child: Icon(
+                  Icons.water_drop_outlined,
+                  size: 80,
+                ),
+              ),
+              const SizedBox(height: 8,),
+              Text(
+                'If you submit code directly copy from chatgpt then mark will 0',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
